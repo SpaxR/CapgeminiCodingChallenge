2de7726af9e5b86a54811bd1bf13afae903f98c4
KaiL <kai.liehr@rwth-aachen.de>
2021-07-29 17:57:01 +0200

    max lüftungsdauer

---
+++ Rules/OpenWindowsNecessary.py
@@ -0,0 +1,61 @@
+import  Rule
+import Api.RoomSensor
+import Api.RoomData
+import Api.BuildingData
+
+
+# Do not open windows more than 20 min in 2 hours
+class OpenWindowsNecessary(Rule):
+
+    # how often should measurements be taken in minutes
+    freq = 5
+
+    # for how long in the past should be looked in minutes
+    interval = 120
+
+    # how many minutes per interval are allowed
+    cap = 20
+
+    # calculates how long the window was opened for at max
+    # for a room based on list of the rooms past data
+    # in minutes
+    def length_of_windows_opened(self, data_room):
+
+        i = 0
+        for data in data_room:
+            if data.sensors.windows_opened:
+                i += 1
+
+        return i * self.freq
+
+    # override
+    def state_optimal(self, rooms, building):
+
+        # find rooms with currently opened windows and
+        # then get their last two hours of data in 5min intervals
+        for room in rooms:
+            if room.sensors.windows_open:
+                # gets data for the room in ???
+                data_room = get_data_api(room, self.interval, self.freq)
+
+                if self.length_of_windows_opened(data_room) > self.cap:
+                    return False
+
+        return True
+
+    # override
+    def path_to_opt(self, rooms, building):
+
+        result = ""
+        # find rooms with currently opened windows and
+        # then get their last two hours of data in 5min intervals
+        for room in rooms:
+            if room.sensors.windows_open:
+                # gets data for the room of the last two hours in 5min intervals
+                data_room = get_data_api(room.id, self.interval, self.freq)
+
+                if self.length_of_windows_opened(data_room) > 20:
+                    result += "Close the windows in room: " + str(room.id)
+
+        return result
+
